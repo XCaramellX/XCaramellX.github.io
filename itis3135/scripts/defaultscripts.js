ca11777853c79b159fbbe37ac30999cb87c7c458
XCaramellX <ndsgirlz@gmail.com>
2022-02-10 13:26:09 -0500

    made a firstjs page

---
+++ itis3135/scripts/defaultscripts.js
@@ -0,0 +1,33 @@
+
+function scriptTest()
+{
+    alert("Hey my script is running!");
+}
+
+function textValues()
+{
+    var a = document.getElementById("formvalues");
+    var text = "";
+    var i;
+    for (i = 0; i < a.length; i++) {
+        text += a.elements[i].value + "<br>";
+    }
+    document.getElementById("tvalues").innerHTML = text;
+
+}
+
+function myTime() {
+    const date = new Date();
+    document.getElementById("datetime").innerHTML = date;
+}
+
+function greetings()
+{
+    let fname = document.getElementById("fname").value;
+    let lname = document.getElementById("lname").value;
+    let feeling = document.getElementById("feeling").value;
+    document.getElementById("nameprompt").innerHTML = "Plum Rabbit welcomes you, " + fname + " " + lname + "!" + " it seems your feeling " + feeling;
+   
+}
+
+
