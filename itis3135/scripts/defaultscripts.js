6dcf416939892bc644e53187ff7d010574213ff9
XCaramellX <ndsgirlz@gmail.com>
2022-02-17 14:33:09 -0500

    update main pages

--- itis3135/scripts/defaultscripts.js
+++ itis3135/scripts/defaultscripts.js
@@ -126,7 +126,8 @@ function getShape()
 
 }
 
-function validateEntry(sides) {
+function validateEntry(sides)
+{
 
     if (sides < 1)
     {
@@ -134,6 +135,207 @@ function validateEntry(sides) {
     }
     
 }
+
+function showNum()
+{
+    var num0 = document.getElementById("0").innerHTML;
+    var num1 = document.getElementById("1").innerHTML;
+    var num2 = document.getElementById("2").innerHTML;
+    var num3 = document.getElementById("3").innerHTML;
+    var num4 = document.getElementById("4").innerHTML;
+    var num5 = document.getElementById("5").innerHTML;
+    var num6 = document.getElementById("6").innerHTML;
+    var num7 = document.getElementById("7").innerHTML;
+    var num8 = document.getElementById("8").innerHTML;
+    var num9 = document.getElementById("9").innerHTML;
+    var num10 = document.getElementById("10").innerHTML;
+
+    if (num0)
+    {
+        alert("0");
+    }
+    if (num1)
+    {
+        alert("1")
+    }
+}
+
+function numkeys()
+{
+    const calculator = document.querySelector('.calculator');
+    const keys = calculator.querySelector('.calculator_keys');
+    var display = document.querySelector('.calculator_display');
+    keys.addEventListener('click', e =>
+    {
+        if (e.target.matches('button'))
+        {
+            const key = e.target;
+            const action = key.dataset.action;
+            const keyContent = key.textContent;
+            var displayedNum = display.textContext;
+            const previousKeyType = calculator.dataset.previousKeyType;
+            if (!action)
+            {
+                if (displayedNum === '0' || previousKeyType === 'operator') {
+                    display.textContent = keyContent;
+                } else {
+                    var tempValue = displayedNum;
+                    tempValue = keyContent;
+                    displayedNum = tempValue;
+                    display.textContent = displayedNum + keyContent;
+                   // display.textContent = displayedNum;
+                }
+                console.log('number key!')
+            }
+            if (action === 'add' || action === 'subtract' || action === 'multiply' || action === 'divide')
+            {
+                key.classList.add('is-depressed');
+                calculator.dataset.previousKeyType = 'operator';
+                console.log('operator key!');
+            }
+            if (action === 'decimal')
+            {
+
+                display.textContent = displayedNum + '.';
+                console.log('decimal key!');
+
+            }
+            if (action === 'clear')
+            {
+                console.log('clear key');
+
+            }
+            if (action === 'calculate')
+            {
+                
+                console.log('equal key!');
+
+            }
+
+            Array.from(key.parentNode.children)
+                .forEach(k => k.classList.remove('is-depressed'))
+            
+        }
+    })
+    
+
+}
+
+window.onload = function ()
+{
+    document.getElementById("addbutton").onclick = numkeys;
+    document.getElementById("subtractbutton").onclick = numkeys;
+    document.getElementById("multiplybutton").onclick = numkeys;
+    document.getElementById("dividebutton").onclick = numkeys;
+    document.getElementById("button7").onclick = numkeys;
+    document.getElementById("button8").onclick = numkeys;
+    document.getElementById("button9").onclick = numkeys;
+    document.getElementById("button4").onclick = numkeys;
+    document.getElementById("button5").onclick = numkeys;
+    document.getElementById("button6").onclick = numkeys;
+    document.getElementById("button1").onclick = numkeys;
+    document.getElementById("button2").onclick = numkeys;
+    document.getElementById("button3").onclick = numkeys;
+    document.getElementById("button0").onclick = numkeys;
+    document.getElementById("decimalbutton").onclick = numkeys;
+    document.getElementById("clearbutton").onclick = numkeys;
+    document.getElementById("calculatebutton").onclick = numkeys;
+
+    //seperate statements for the arrays assignment
+    document.getElementById("results_table").onclick = displaySalary;
+    document.getElementById("salary").onclick = addSalary;
+
+}
+
+var person = [];
+var salaries = [];
+
+function addSalary()
+{
+    
+    for (var i = 0; i <= 5; i++)
+    {
+        person[i] = document.getElementById("employee").value;
+        for (var j = 0; j <= 3; j++)
+        {
+            if (person[i] == "Carter Ellis")
+            {
+                salaries[j] = " "  + '$4000';
+               
+                
+            }
+            if (person[i] == "David West")
+            {
+                salaries[j] = " " + '$4000';
+                
+            }
+            if (person[i] == "Hope Bardot")
+            {
+                salaries[j] = " " + '$4000';
+                
+            }
+
+            if (person[i] == "Jacob Lopez")
+            {
+
+                salaries[j] = " " + '$6000';
+                
+            }
+            if (person[i] == "Jaden Adams")
+            {
+                salaries[j] = " " + '$7000';
+                
+            }
+        }
+
+        
+    }
+    
+
+    
+}
+
+function displaySalary()
+{
+    var table = document.createElement("table")
+    var tableRows = table.insertRow();
+    var cell;
+    var eachrow = 1;
+   person.forEach((value) =>
+    {
+        cell = tableRows.insertCell();
+        cell.innerHTML = value;
+        var nextEntry = 1;
+        if (nextEntry % eachrow == 0 && nextEntry != person.length)
+        {
+
+            tableRows = table.insertRow();
+        }
+
+   });
+    salaries.forEach((value) => {
+        cell = tableRows.insertCell();
+        cell.innerHTML = value;
+        var nextEntry =  1;
+        if (nextEntry % eachrow == 0 && nextEntry != person.length) {
+
+            tableRows = table.insertRow();
+        }
+
+    });
+   
+    document.getElementById("results_table").appendChild(table);
+}
+
+
+
+
+
+
+
+
+
+
 
 
 
