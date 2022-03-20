e1b03863ac352046c1049f4619de54d17aa3631e
XCaramellX <ndsgirlz@gmail.com>
2022-03-20 04:24:42 -0400

    updated chess project

--- itis3135/projects/project_scripts/chess_project.js
+++ itis3135/projects/project_scripts/chess_project.js
@@ -1,9 +1,20 @@
-function pagePopUp()
+
+var kingButton = document.getElementById("king");
+var kingModal = document.getElementById("kingmodal");
+
+
+window.onclick = function(event)
 {
+    if (event.target == kingButton)
+    {
+        kingModal.style.display = "block";
+    }
+
+    if (event.target == kingModal)
+    {
+        kingModal.style.display = "none";
+    }
+
     
 }
 
-window.onload = function ()
-{
-    document.getElementById("king").onclick = pagePopUp;
-}
