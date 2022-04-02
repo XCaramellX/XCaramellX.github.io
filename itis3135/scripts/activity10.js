431f731372b050bb3e3924903a9f9717057f048c
XCaramellX <ndsgirlz@gmail.com>
2022-04-01 20:31:46 -0400

    updated image swap and learning more about flexbox

--- itis3135/scripts/activity10.js
+++ itis3135/scripts/activity10.js
@@ -1,15 +1,30 @@
 $(document).ready(function() {
+        // get the image URL and caption for each image and animate the caption
+        $("image_list a").each(function () {
+                let imageURL = $(this).attr("href");
+                let caption = $(this).attr("title");
 
+                // preload the image for each link
+                let beachGallery = new Image();
+                beachGallery.src = imageURL;
 
-        // preload the image for each link
+                // set up the event handlers for each link
+                $(this).click(function(event){
+                        $("caption, #image").slideUp(2000,
+                        function() {
+                        $("#image").attr("src", imageURL);
+                        $("#caption").text(caption);
+                        $("#caption, #image").slideDown(2000);
 
-        // set up the event handlers for each link
+                         });
+                         // cancel the default action of each link
+                         event.preventDefault();
+                });
 
-    			// get the image URL and caption for each image and animate the caption
+        
+        });
+        // move the focus to the first link
+        $("li:first-child a").focus();
 
-            // cancel the default action of each link
-
-
-    // move the focus to the first link
-
+        
 }); // end ready
