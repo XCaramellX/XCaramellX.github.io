80fa5e4178071e2e883bcf806dd5e6a30e815d20
XCaramellX <ndsgirlz@gmail.com>
2022-04-04 17:46:07 -0400

    updated peer reviews, tables, and activity 10

--- itis3135/scripts/activity10.js
+++ itis3135/scripts/activity10.js
@@ -1,29 +1,30 @@
 $(document).ready(function() {
         // get the image URL and caption for each image and animate the caption
-        $("image_list a").each(function () {
+        $("#image_list a").each(function () {
                 
                 let caption = $(this).attr("title");
 
                 // preload the image for each link
                 let beachGallery = new Image();
+                beachGallery.src = $(this).attr("href");
+        });
                
 
                 // set up the event handlers for each link
-                $(this).click(function(event){
-                        $("caption, #image").slideUp(2000,
-                        function() {
-                        $("#image").attr("src");
+                $("#image_list a").click(function(event){
+                        let imageURL = $(this).attr("href");
+                        $("#image").attr("src", imageURL);
+
+                        let caption = $(this).attr("title");
                         $("#caption").text(caption);
-                        $("#caption, #image").slideDown(2000);
+
+                        // cancel the default action of each link
+                        event.preventDefault();
 
                          });
-                         // cancel the default action of each link
-                         event.preventDefault();
-                });
-
-        
-        });
-        // move the focus to the first link
+                         
+                         
+                  // move the focus to the first link
         $("li:first-child a").focus();                                                                                                                                                                                 
 
         
