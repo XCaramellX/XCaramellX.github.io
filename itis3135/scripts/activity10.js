778685e3033eac510be8d6b817dae6eeccacfaa5
XCaramellX <ndsgirlz@gmail.com>
2022-04-04 11:56:30 -0400

    update activity 10

--- itis3135/scripts/activity10.js
+++ itis3135/scripts/activity10.js
@@ -1,18 +1,18 @@
 $(document).ready(function() {
         // get the image URL and caption for each image and animate the caption
         $("image_list a").each(function () {
-                let imageURL = $(this).attr("href");
+                
                 let caption = $(this).attr("title");
 
                 // preload the image for each link
                 let beachGallery = new Image();
-                beachGallery.src = imageURL;
+               
 
                 // set up the event handlers for each link
                 $(this).click(function(event){
                         $("caption, #image").slideUp(2000,
                         function() {
-                        $("#image").attr("src", imageURL);
+                        $("#image").attr("src");
                         $("#caption").text(caption);
                         $("#caption, #image").slideDown(2000);
 
@@ -24,7 +24,7 @@ $(document).ready(function() {
         
         });
         // move the focus to the first link
-        $("li:first-child a").focus();
+        $("li:first-child a").focus();                                                                                                                                                                                 
 
         
 }); // end ready
